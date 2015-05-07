eefe2d6eaf6a2b7ca21ec92014640a4859837597
Jorge Lopez (iMac) <jorge@developez.org>
2015-05-07 12:53:28 +0200

    p

--- app/connect.php
+++ app/connect.php
@@ -1,3 +1,3 @@
 <?php
-return new PDO('mysql:host=localhost;dbname=clinicas','root','root',array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+return new PDO('mysql:host=http://www.infenlaces.com;dbname=jorge_lopez_datatables','jorge_lopez_usr','datatablesajax',array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 ?>
